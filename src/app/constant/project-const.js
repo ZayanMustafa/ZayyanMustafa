3f96f44ba9cc71957ee45e3105885e12b5ef074a
Zayan Mustafa <mustafazayyan13@gmail.com>
2025-03-18 08:46:34 +0500

    Projects Update

--- src/app/constant/project-const.js
+++ src/app/constant/project-const.js
@@ -3,19 +3,25 @@ export const projects = [
   {
     id: 1,
     name: "Zay Chat App",
-    description:
-      "A real-time chat app using React, Firebase, and Material UI. Users can sign in with email and send messages. The app features a responsive design and a clean user interface. User can send messages to any user their conversation is private but it can view them in real-time.",
+    description: "A real-time chat app using React, Firebase, and Material UI. Users can sign in with email and send messages. The app features a responsive design and a clean user interface. User can send messages to any user their conversation is private but it can view them in real-time.",
     image: "/placeholder.svg?height=400&width=500",
     imageAlt: "Chat application with user interface. User can send a message to any user their conversation is private but it can view them in real time.",
     url: "https://zaychat.vercel.app/",
   },
   {
     id: 2,
-    name: "Real time climate generator",
-    description:
-      "What was your role, your deliverables, if the project was personal, freelancing.",
+    name: "Weather App",
+    description: "The weather app that fetches the weather data from the OpenWeatherMap API. This project is made using JavaScript, Bootstrap, and API Integration.",
     image: "/placeholder.svg?height=400&width=500",
-    imageAlt: "Calendar with yellow highlighter and pen",
-    url: "http://localhost:3000/projects",
+    imageAlt: "Weather application with user interface. User can view the weather data from the OpenWeatherMap API.",
+    url: "https://zayanmustafa.github.io/ClimateTracker/",
   },
+  {
+    id: 3,
+    name: "To do Application",
+    description:"To do app that allows users to add and delete tasks. This project was made using JavaScript and Bootstrap.",
+    image: "/placeholder.svg?height=400&width=500",
+    imageAlt: "To do application that allows users to add and delete tasks. This project was made using JavaScript and Bootstrap.",
+    url: "https://zayanmustafa.github.io/TaskTracker/",
+  }
 ];
--- src/app/constant/projectdetail-const.js
+++ src/app/constant/projectdetail-const.js
@@ -2,23 +2,62 @@
 export const projects = [
     {
       id: 1,
-      name: "Project 1",
-      description: "A brief description of Project 1.",
-      techStack: ["React", "Next.js", "Tailwind CSS"],
-      githubLink: "https://github.com/yourusername/project1",
-      deployLink: "https://project1.com",
-      youtubeLink: "https://youtube.com/project1",
+      name: "Real time Chat Application",
+      description: "A real-time chat app using React, Firebase, and Tailwind CSS. Users can sign in with email and send messages. The app features a responsive design and a clean user interface. User can send messages to any user their conversation is private but it can view them in real-time.",
+      techStack: ["React", "FireBase", "Tailwind CSS"],
+      githubLink: "https://github.com/ZayanMustafa/ZayChat",
+      deployLink: "https://zaychat.vercel.app/",
+      youtubeLink: "https://youtu.be/UKUWGNomdOA",
       image: "/project1.png",
     },
     {
       id: 2,
-      name: "Project 2",
-      description: "A brief description of Project 2.",
-      techStack: ["Node.js", "Express", "MongoDB"],
-      githubLink: "https://github.com/yourusername/project2",
-      deployLink: "https://project2.com",
-      youtubeLink: "https://youtube.com/project2",
+      name: "Quote Generator",
+      description: "A random quote generator that fetches quotes from an MockDataSource. This is a simple project that I made to practice in fetching Data. This project is made using TypeScript.", 
+      techStack: ["TypeScript", "Data Fetching", "Bootstrap"],
+      githubLink: "https://github.com/ZayanMustafa/QuipQuake-Generator",
+      deployLink: "https://zayanmustafa.github.io/QuipQuake-Generator/",
+      youtubeLink: "https://youtu.be/lXuQTzXY2jg?si=MUBheWiF0LFjvXv2",
       image: "/project2.png",
     },
-    // Add more projects as needed
+    {
+      id: 3,
+      name: "Real time Task Tracker",
+      description:"A task tracker app that allows users to add and delete tasks. This project was made using React, and Bootstrap.",
+      techStack: ["HTML", "CSS", "JavaScript" ,"LocalStorage"],
+      githubLink: "https://github.com/ZayanMustafa/TaskTracker",
+      deployLink: "https://zayanmustafa.github.io/TaskTracker/",
+      youtubeLink: "https://youtu.be/hRBmr_Xv-PY?si=HZRMqZ8BoFkxyuYw",
+      image: "/project3.png",
+    },
+    {
+      id: 4,
+      name: "Weather App",
+      description: "The weather app that fetches the weather data from the OpenWeatherMap API. This project is made using JavaScript, Bootstrap, and API Integration. ", 
+      techStack: ["JavaScript", "Bootstrap" , "API Integration" ],
+      githubLink: "https://github.com/ZayanMustafa/ClimateTracker",
+      deployLink: "https://zayanmustafa.github.io/ClimateTracker/",   
+      youtubeLink: "https://youtu.be/w048696K_9A", // Need to Update
+      image: "/project4.png",
+    },
+    {
+      id: 5,
+      name: "Digital Clock",
+      description: " A simple digital clock that displays the current time. This project was made using JavaScript, Bootstrap, and Responsive Design and color theme for different size of screen.",
+      techStack: ["JavaScript", "Bootstrap", "Responsive Design"],
+      githubLink: "https://github.com/ZayanMustafa/DigitalClock",
+      deployLink: "https://zayanmustafa.github.io/DigitalClock/",
+      youtubeLink: "https://youtu.be/9-X49z-a38U", // Need to Update
+      image: "/project5.png",
+    },
+    {
+      id: 6,
+      name:"Lock Builder",
+      description:"The lock builder that allows users to build their own strong lock. This project was made using JavaScript, Bootstrap, and Responsive Design.",
+      techStack: ["JavaScript", "Bootstrap", "Responsive Design"],
+      githubLink: "https://github.com/ZayanMustafa/LockBuilder",
+      deployLink: "https://zayanmustafa.github.io/LockBuilder/",
+      youtubeLink: "https://youtu.be/9-X49z-a38U", // Need to Update
+    }
+      // Add more projects as needed
   ];
