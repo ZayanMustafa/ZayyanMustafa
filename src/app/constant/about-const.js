9281f750697aa759c0b07557689c56f26ab62bc4
Zayyan Mustafa <160398419+ZayanMustafa@users.noreply.github.com>
2025-03-22 02:22:25 +0500

    Update about-const.js

--- src/app/constant/about-const.js
+++ src/app/constant/about-const.js
@@ -4,7 +4,7 @@ export const portfolioData = {
   about: {
     title: "About me",
     description:
-      "I'm Zayyan Mustafa, a dedicated Front-End Developer with a strong focus on building interactive, user-friendly, and performance-driven web applications. My expertise includes React, Next.js, Firebase, and Material UI, allowing me to create sleek and efficient digital experiences.\n\nI have earned Google certifications, enhancing my technical skill set. My projects include a Summit View School website featuring Firebase authentication and a custom LMS system with role-based access.\n\nBeyond tech, I actively contribute to Islamic and educational initiatives, leveraging technology for meaningful impact. My goal is to create solutions that are both innovative and user-centric. Let’s build something amazing!",
+      "I'm Zayyan Mustafa, a dedicated Front-End Developer with a strong focus on building interactive, user-friendly, and performance-driven web applications. My expertise includes React, Next.js, Firebase, and Material UI, allowing me to create sleek and efficient digital experiences.\n\nI have earned Google certifications, enhancing my technical skill set. My projects include a Summit View School website featuring Firebase authentication and a custom LMS system with role-based access.\n\nBeyond tech, I actively contribute to Islamic and educational initiatives, leveraging technology for meaningful impact. My goal is to create solutions that are both innovative and user-centric.",
     resumeUrl: "/zayyanmustafaresume.pdf",
   },
   profileImage: "/remove.png",
